3db98c9bf72d9f904e1ebb7f4591ddaaaebec3b5
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 23:24:08 +0200

    feat: add WordPassMode/WordPassStatus type aliases in core/common

--- AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/wordpass/GameWordPassEnums.kt
+++ AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/wordpass/GameWordPassEnums.kt
@@ -1,3 +1,5 @@
 package es.sebas1705.common.games.wordpass
 
 typealias Letter = es.sebas1705.resources.games.wordpass.Letter
+typealias WordPassMode = es.sebas1705.resources.games.wordpass.WordPassMode
+typealias WordPassStatus = es.sebas1705.resources.games.wordpass.WordPassStatus
