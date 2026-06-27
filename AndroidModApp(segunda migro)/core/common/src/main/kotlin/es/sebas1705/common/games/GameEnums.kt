5d92234ef82215e914c3aa5701c12d5db989a895
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 18:15:29 +0200

    feat: add game enum type aliases to common.games package

---
+++ AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/GameEnums.kt
@@ -0,0 +1,6 @@
+package es.sebas1705.common.games
+
+typealias Category = es.sebas1705.resources.games.Category
+typealias Difficulty = es.sebas1705.resources.games.Difficulty
+typealias Languages = es.sebas1705.resources.games.Languages
+typealias QuizType = es.sebas1705.resources.games.QuizType
