bf126b5a305f28ada571d9c031302f38ea47bfdf
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 23:49:47 +0200

    fix: correct type alias packages to es.sebas1705.core.resources in GameQuizEnums.kt

--- AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/quiz/GameQuizEnums.kt
+++ AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/quiz/GameQuizEnums.kt
@@ -1,4 +1,4 @@
 package es.sebas1705.common.games.quiz
 
 typealias QuizMode = es.sebas1705.core.resources.games.quiz.QuizMode
-typealias QuizStatus = es.sebas1705.resources.games.quiz.QuizStatus
+typealias QuizStatus = es.sebas1705.core.resources.games.quiz.QuizStatus
