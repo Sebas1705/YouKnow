4db580e9be108524db682ff81c622333b524be01
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 23:34:00 +0200

    fix: add missing SoundPool constants (PRIORITY_SOUND, LOOP_N, RATE) to ComposableConstants

--- AndroidModApp(segunda migro)/core/designsystem/src/main/kotlin/es/sebas1705/designsystem/ComposableConstants.kt
+++ AndroidModApp(segunda migro)/core/designsystem/src/main/kotlin/es/sebas1705/designsystem/ComposableConstants.kt
@@ -2,21 +2,8 @@ package es.sebas1705.designsystem
 
 import es.sebas1705.core.designsystem.R
 
-
-
 /**
  * Constants of the composable package
- *
- * @property MAX_GROUP [Int]: Max number of groups that can be created
- * @property MAX_SOUNDS_SIMULTANEITY [Int]: Max number of sounds that can be played simultaneously
- * @property BUTTON_SOUND [Int]: Button sound
- * @property ICON_BUTTON_SOUND [Int]: Icon button sound
- * @property FAB_BUTTON_SOUND [Int]: FAB button sound
- * @property RADIO_BUTTON_SOUND [Int]: Radio button sound
- * @property NAV_BUTTON_SOUND [Int]: Navigation button sound
- * @property GAME_BUTTON_SOUND [Int]: Game button sound
- * @property WIN_SOUND [Int]: Win sound
- * @property LOSE_SOUND [Int]: Lose sound
  *
  * @author Sebas1705 12/09/2025
  * @since 1.0.0
@@ -24,6 +11,18 @@ import es.sebas1705.core.designsystem.R
 object ComposableConstants {
     const val MAX_GROUP = 50
 
-    //Sounds:
+    //SoundPool play params:
+    const val PRIORITY_SOUND = 1
+    const val LOOP_N = 0
+    const val RATE = 1.0f
 
+    //Sound ids:
+    const val BUTTON_SOUND = R.raw.sound_click
+    const val ICON_BUTTON_SOUND = R.raw.sound_click
+    const val FAB_BUTTON_SOUND = R.raw.sound_click
+    const val RADIO_BUTTON_SOUND = R.raw.sound_click
+    const val NAV_BUTTON_SOUND = R.raw.sound_click
+    const val GAME_BUTTON_SOUND = R.raw.sound_click
+    const val WIN_SOUND = R.raw.sound_win
+    const val LOSE_SOUND = R.raw.sound_lose
 }
