71f8940e621f24fded81d225b42a1a735c0c9f3e
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 23:24:04 +0200

    feat: add FamiliesMode/FamiliesStatus type aliases in core/common

---
+++ AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/families/GameFamiliesEnums.kt
@@ -0,0 +1,4 @@
+package es.sebas1705.common.games.families
+
+typealias FamiliesMode = es.sebas1705.core.resources.games.families.FamiliesMode
+typealias FamiliesStatus = es.sebas1705.resources.games.families.FamiliesStatus
