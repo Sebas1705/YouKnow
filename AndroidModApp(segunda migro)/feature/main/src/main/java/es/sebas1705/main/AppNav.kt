8c480edb19fdb6362a9cd40b0723814602f3a186
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-28 00:35:37 +0200

    fix: give AppNav default param values so MainActivity can call AppNav() with no args

--- AndroidModApp(segunda migro)/feature/main/src/main/java/es/sebas1705/main/AppNav.kt
+++ AndroidModApp(segunda migro)/feature/main/src/main/java/es/sebas1705/main/AppNav.kt
@@ -1,17 +1,19 @@
 package es.sebas1705.main
 
-
 import android.media.SoundPool
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import es.sebas1705.auth.AuthNav
 import es.sebas1705.common.states.WindowState
+import es.sebas1705.designsystem.states.rememberWindowState
 import es.sebas1705.game.GameNav
 import es.sebas1705.guide.GuideScreen
 import es.sebas1705.home.navigation.HomeNav
@@ -19,23 +21,12 @@ import es.sebas1705.home.play.GameItem.Companion.games
 import es.sebas1705.settings.SettingsScreen
 import es.sebas1705.survey.SurveyScreen
 
-/**
- * Navigation for the app.
- *
- * @param startDestination [AppGraph]: Start destination of the app.
- * @param windowState [WindowState]: State of the window.
- * @param onMusicChange (Boolean) -> Unit: Function that will change the music.
- * @param soundPool [Pair]<[SoundPool], [Float]>: Pair of the SoundPool and the volume.
- *
- * @author Sebas1705 12/09/2025
- * @since 1.0.0
- */
 @Composable
 fun AppNav(
-    startDestination: AppGraph,
-    windowState: WindowState,
-    onMusicChange: (Boolean) -> Unit,
-    soundPool: Pair<SoundPool, Float>,
+    startDestination: AppGraph = AppGraph.SplashScreen,
+    windowState: WindowState = WindowState.default(),
+    onMusicChange: (Boolean) -> Unit = {},
+    soundPool: Pair<SoundPool, Float>? = null,
 ) {
     // NavController:
     val appNavController = rememberNavController()
@@ -45,7 +36,5 @@ fun AppNav(
 
     //Body:
     NavHost(navController = appNavController, startDestination = startDestination) {
-
     }
 }
-
