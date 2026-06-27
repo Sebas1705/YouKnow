b44401a50ff450b4f498d1af13ee69e7d70deb99
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 17:50:01 +0200

    fix: remove non-existent MyDocumentFirestoreDataSource template stub

--- AndroidModApp(segunda migro)/data/repositories/src/main/java/es/sebas1705/repositories/repos/FirestoreRepository.kt
+++ AndroidModApp(segunda migro)/data/repositories/src/main/java/es/sebas1705/repositories/repos/FirestoreRepository.kt
@@ -3,24 +3,4 @@ package es.sebas1705.repositories.repos
 import es.sebas1705.repositories.interfaces.IFirestoreRepository
 import javax.inject.Inject
 
-/**
- * Implementation of IFirestoreRepository.
- *
- * @property myDocumentFirestoreDataSource [MyDocumentFirestoreDataSource]: Data source for managing documents in Firestore.
- * @since 0.1.0
- * @author Sebas1705 09/09/2025
- */
-class FirestoreRepository @Inject constructor(
-    private val myDocumentFirestoreDataSource: MyDocumentFirestoreDataSource
-) : IFirestoreRepository {
-
-
-    //Listeners:
-
-
-    //References:
-
-
-    //Listeners:
-
-}
+class FirestoreRepository @Inject constructor() : IFirestoreRepository
