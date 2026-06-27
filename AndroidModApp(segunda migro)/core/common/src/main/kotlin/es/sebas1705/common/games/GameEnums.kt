16f7d5651f3a0ee614cb4bb5721d863629d310ae
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 23:49:43 +0200

    fix: correct type alias packages to es.sebas1705.core.resources in GameEnums.kt

--- AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/GameEnums.kt
+++ AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/GameEnums.kt
@@ -1,6 +1,6 @@
 package es.sebas1705.common.games
 
-typealias Category = es.sebas1705.resources.games.Category
-typealias Difficulty = es.sebas1705.resources.games.Difficulty
-typealias Languages = es.sebas1705.resources.games.Languages
-typealias QuizType = es.sebas1705.resources.games.QuizType
+typealias Category = es.sebas1705.core.resources.games.Category
+typealias Difficulty = es.sebas1705.core.resources.games.Difficulty
+typealias Languages = es.sebas1705.core.resources.games.Languages
+typealias QuizType = es.sebas1705.core.resources.games.QuizType
