7644811b99702c9d36eccb1391edc59e31f447ba
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 23:49:46 +0200

    fix: correct type alias packages to es.sebas1705.core.resources in GameMysteryNumberEnums.kt

--- AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/mysterynumber/GameMysteryNumberEnums.kt
+++ AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/mysterynumber/GameMysteryNumberEnums.kt
@@ -1,5 +1,5 @@
 package es.sebas1705.common.games.mysterynumber
 
-typealias MysteryNumberMode = es.sebas1705.resources.games.mysterynumber.MysteryNumberMode
-typealias MysteryNumberStatus = es.sebas1705.resources.games.mysterynumber.MysteryNumberStatus
-typealias Numbers = es.sebas1705.resources.games.mysterynumber.Numbers
+typealias MysteryNumberMode = es.sebas1705.core.resources.games.mysterynumber.MysteryNumberMode
+typealias MysteryNumberStatus = es.sebas1705.core.resources.games.mysterynumber.MysteryNumberStatus
+typealias Numbers = es.sebas1705.core.resources.games.mysterynumber.Numbers
