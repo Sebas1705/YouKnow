05d3e17571b9a983ad34f958257484ea2a829bc3
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 23:24:06 +0200

    feat: add MysteryNumber type aliases in core/common

---
+++ AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/games/mysterynumber/GameMysteryNumberEnums.kt
@@ -0,0 +1,5 @@
+package es.sebas1705.common.games.mysterynumber
+
+typealias MysteryNumberMode = es.sebas1705.resources.games.mysterynumber.MysteryNumberMode
+typealias MysteryNumberStatus = es.sebas1705.resources.games.mysterynumber.MysteryNumberStatus
+typealias Numbers = es.sebas1705.resources.games.mysterynumber.Numbers
