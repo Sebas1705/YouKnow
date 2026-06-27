97c228b474d0f044de9fbfe811bdcff21367f686
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-27 18:33:51 +0200

    feat: add MVIBaseViewModel

---
+++ AndroidModApp(segunda migro)/core/common/src/main/kotlin/es/sebas1705/common/classes/mvi/MVIBaseViewModel.kt
@@ -0,0 +1,33 @@
+package es.sebas1705.common.classes.mvi
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+abstract class MVIBaseViewModel<S : MVIBaseState, I : MVIBaseIntent> : ViewModel() {
+
+    private val _uiState: MutableStateFlow<S> by lazy { MutableStateFlow(initState()) }
+    val uiState: StateFlow<S> by lazy { _uiState.asStateFlow() }
+
+    abstract fun initState(): S
+    abstract fun intentHandler(intent: I)
+
+    fun sendIntent(intent: I) = intentHandler(intent)
+
+    protected fun updateUi(transform: (S) -> S) {
+        _uiState.update(transform)
+    }
+
+    protected fun execute(
+        dispatcher: CoroutineDispatcher = Dispatchers.Main,
+        action: suspend () -> Unit
+    ) {
+        viewModelScope.launch(dispatcher) { action() }
+    }
+}
